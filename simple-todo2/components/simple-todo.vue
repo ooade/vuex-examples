e3aa7c4d4902b6321366f3f36af38a76cc24bd80
Hugo Castro <ispg4243@ispgaya.pt>
2022-03-09 10:35:20 +0000

    ficheiro atualizado

--- simple-todo2/components/simple-todo.vue
+++ simple-todo2/components/simple-todo.vue
@@ -17,8 +17,10 @@
 				<div class="grid-item " >ssid </div>
 			</div>
 
-			<div class="grid-container" v-for="(anchor) in filter" 
-					:key="anchor.id">
+			<div class="grid-container" v-for="(anchor,index) in filter" 
+					:key="index"
+					
+					>
 				<div class="grid-item" >{{ anchor.bssid }} </div>
 				<div class="grid-item" >{{ anchor.fw_version }} </div>
 				<div class="grid-item" >{{ anchor.id.toString(16).toUpperCase().match(/.{1,2}/g).join(":") }} </div>
@@ -27,6 +29,28 @@
 				<div class="grid-item" >{{ anchor.rssi }} </div>
 				<div class="grid-item" >{{ anchor.runtime }} </div>
 				<div class="grid-item" >{{ anchor.ssid }} </div>
+			</div>
+
+			<h1>Tags</h1>
+			<div class="grid-container2" >	
+				<div class="grid-item "> active </div>
+				<div class="grid-item " >battery </div>
+				<div class="grid-item " >id </div>
+				<div class="grid-item " >label </div>
+				<div class="grid-item " >last_anchor </div>
+				<div class="grid-item ">last_seen_at </div>
+
+			</div>
+			<div class="grid-container2" v-for="(tag, index) in filterTags" 
+					:key="index"
+					
+					>
+				<div class="grid-item" >{{ tag.active }} </div>
+				<div class="grid-item" >{{ tag.battery }} </div>
+				<div class="grid-item" >{{ tag.id.toString(16).toUpperCase().match(/.{1,2}/g).join(":")}} </div>
+				<div class="grid-item" >{{ tag.label }} </div>
+				<div class="grid-item" >{{ tag.last_anchor.toString(16).toUpperCase().match(/.{1,2}/g).join(":") }} </div>
+				<div class="grid-item" >{{ tag.last_seen_at }} </div>
 			</div>
 
 		</div>
@@ -43,8 +67,7 @@
 		data() {
 			return {
 				search:"",
-				Lista: [],
-
+				
 				orders: {
 					bssid: true,
 					label: true,
@@ -56,20 +79,47 @@
 		},
 
 		computed: { 
+
 			anchors: () => store.state.list,
+			tags: () => store.state.list2,
 
 			filter(){
 				return this.anchors.filter( anchors => {
-					return anchors.label.match(this.search);
+					return anchors.id.toString(16).toUpperCase().match(/.{1,2}/g).join(":").match(this.search) || anchors.label.match(this.search) ;
 				})
-			}
+			},
 
+			filterTags(){
+
+				let tempArr = []
+
+				if(this.search===""){
+					return tempArr;
+
+				}else{
+					
+					this.tags.forEach(tag => {
+
+						this.filter.forEach(element => {
+							if(tag.last_anchor === element.id){
+								tempArr.push(tag)
+							}
+						});
+
+					});
+					return tempArr;
+				}
+			},
 		},
 
 		methods: {
 
 			List() {	
 				store.commit('List')
+			},
+
+			List2() {	
+				store.commit('List2')
 			},
 
 			myFunction(ref) { 
@@ -172,7 +222,12 @@
 		},
 
 		created() {
-			this.List();
+
+			if(this.filter.length){
+			}else{
+				this.List();
+				this.List2();
+			}
 		},
 	}
 
@@ -207,5 +262,20 @@
   	text-align: center;
 }
 
+.grid-container2 {
+  	display: grid;
+  	grid-template-columns:  16.7% 16.7% 16.7% 16.7% 16.7% 16.7%;
+  	background-color: #2196F3;
+  	padding: 1px;
+}
+.grid-item2 {
+  	background-color: rgba(255, 255, 255, 0.8);
+  	border: 1px solid rgba(0, 0, 0, 0.8);
+  	padding: 30px;
+  	font-size: 18px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+  	text-align: center;
+}
 
 </style>
--- simple-todo2/store/index.js
+++ simple-todo2/store/index.js
@@ -10,9 +10,12 @@ const URL ="https://staging-at-api.azitek.io/"
 export default new Vuex.Store({
 	state: {
 		list: [],
+		list2: [],
+	
 	},
 
 	mutations: {
+		
 		List(state) {
 			axios.get(`${URL}anchors/`, {
 				headers: {
@@ -21,7 +24,6 @@ export default new Vuex.Store({
 			})
 
 			.then(resp=>{
-				console.log(resp.data)
 				
 				resp.data.forEach(value => {
 									
@@ -33,6 +35,24 @@ export default new Vuex.Store({
 					state.list.push(value)
 					
 
+				});
+			})
+			this.var=true
+		},
+
+		List2(state) {
+			axios.get(`${URL}tags/`, {
+				headers: {
+					'X-Api-Key': "6e133fd221f87a77545181af132e7b5c2cee09c328a8f310d513dc9e89c8f3f3"
+				}
+			})
+
+			.then(resp=>{
+				
+				resp.data.forEach(value => {
+					value.last_seen_at = moment.unix(value.last_seen_at).format('MMMM Do YYYY, h:mm:ss')		
+					state.list2.push(value)
+					
 				});
 			})
 		},
